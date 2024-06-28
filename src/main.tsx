f72a1dba7f3a97c1f524fdf4cb87cd562cfa0b93
user <wkd5878@gmail.com>
2024-06-28 20:37:59 +0900

    chore: prettier 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './scss/index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./scss/index.scss";
 
-import App from './components/pages/App';
-import Home from './components/pages/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from "./components/pages/App";
+import Home from "./components/pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [{ index: true, element: <Home /> }],
   },
@@ -18,10 +18,10 @@ const routes = [
 const router = createBrowserRouter(routes);
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- src/test/jest.test.ts
+++ src/test/jest.test.ts
@@ -1,10 +1,10 @@
-describe('js test', () => {
-  it('이건 테스트 타이틀 메시지1', () => {
+describe("js test", () => {
+  it("이건 테스트 타이틀 메시지1", () => {
     expect(3 + 4).toBe(7);
   });
 
-  it('이건 테스트 타이틀 메시지2', () => {
-    const name = 'yuds';
-    expect(name).toBe('yuds');
+  it("이건 테스트 타이틀 메시지2", () => {
+    const name = "yuds";
+    expect(name).toBe("yuds");
   });
 });
