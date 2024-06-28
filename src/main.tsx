6f52f25c8da1af8030696681215a44c2c4ed7143
japark <wkd5878@gmail.com>
2024-06-28 14:54:26 +0900

    init: prettier 설치(dev) 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 
 import App from './components/pages/App';
-import Home from "./components/pages/Home";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import Home from './components/pages/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const routes = [{
-    path: "/",
-    element: <App/>,
-    children: [
-        {index: true, element: <Home/>}
-    ]
-}];
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    children: [{ index: true, element: <Home /> }],
+  },
+];
 
 const router = createBrowserRouter(routes);
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router}/>
-        </QueryClientProvider>
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
