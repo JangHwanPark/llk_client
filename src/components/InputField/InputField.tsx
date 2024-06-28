44757190ac2b78a60928d93905c9c73bb3ed6c8d
japark <wkd5878@gmail.com>
2024-06-28 17:51:26 +0900

    feat: InputField.tsx 컴포넌트 생성 및 axios 인스턴스 생성파일 네이밍 변경

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+interface InputFieldProps {
+  id?: string;
+  text?: string;
+  style?: React.CSSProperties;
+  placeholder: string;
+  type: 'text' | 'password' | 'email' | 'number';
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  ariaLabel?: string;
+  ariaDescribedby?: string;
+}
+
+export const InputField = ({
+  id,
+  text,
+  style,
+  placeholder,
+  type,
+  onChange,
+  value,
+  ariaLabel,
+  ariaDescribedby,
+}) => {
+  return (
+    <div>
+      <label htmlFor={id}>
+        <span>{text}</span>
+        <input
+          id={id}
+          className="input-field"
+          type={type}
+          placeholder={placeholder}
+          style={style}
+          onChange={onChange}
+          value={value}
+          aria-label={ariaLabel}
+          aria-describedby={ariaDescribedby}
+        />
+      </label>
+    </div>
+  );
+};
