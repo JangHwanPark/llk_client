aaefea4294befaf53f33f6ff91d069ad0a019c3a
japark <wkd5878@gmail.com>
2024-06-28 17:21:36 +0900

    feat: css 설정 및 주석정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/index.scss';
-
-import App from './components/pages/App';
-import Home from './components/pages/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// import pages
+import App from './pages/Home/App';
+import Home from './pages/Home/Home';
+
+// import css
+import './assets/scss/index.scss';
+
+// 라우터 설정
 const routes = [
   {
     path: '/',
@@ -15,6 +19,7 @@ const routes = [
   },
 ];
 
+// 라우터 생성 및 쿼리클라이언트 생성
 const router = createBrowserRouter(routes);
 const queryClient = new QueryClient();
 
