6b91317c3017c4e200fb91c7f2087938832a9f4d
japark <wkd5878@gmail.com>
2024-07-01 18:22:49 +0900

    feat: 로그인 페이지 마크업

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {InputField} from "../../components/InputField/InputField";
+import {Link} from "react-router-dom";
 
 export default function SignIn() {
   return (
     <div>
-      
+      <section>
+        <h1>Login</h1>
+      </section>
+      <form>
+        <div>
+          <InputField type="text" placeholder="이름"/>
+          <InputField type="text" placeholder="비밀번호"/>
+        </div>
+        <button>로그인</button>
+        <div className="division"></div>
+        <div>
+          <button>구글 로그인</button>
+        </div>
+      </form>
+      <div>
+        <p>계정이 없으신가요? <Link to="/signup">회원가입</Link></p>
+        <p>비밀번호를 잊으셨나요 ?</p>
+      </div>
     </div>
   );
 }
