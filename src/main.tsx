a0f65e7cccf0b67fa38d0568c793bfc8a9cc3e81
user <wkd5878@gmail.com>
2024-07-02 22:10:37 +0900

    chore: 폴더구조 재정의

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // import pages
-import App from './pages/Home/App';
-import Home from './pages/Home/Home';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
+import App from './pages/App';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 // import css
-import './assets/scss/index.scss';
+import './assets/styles/index.scss';
 
 // 라우터 설정
 const routes = [
