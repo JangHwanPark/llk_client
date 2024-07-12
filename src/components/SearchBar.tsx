56dbd3bca510cff524e2cac4c38d7100f2320c17
user <wkd5878@gmail.com>
2024-07-13 07:03:38 +0900

    feat: 히어로 섹션 임시 디자인

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <div>
+      <div>
+        <span>Buy</span>
+        <span>Sell</span>
+        <span>Rent</span>
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
-import LoadingSpinner from '../components/LoadingSpinner';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
     <div className="container">
       <Navigation/>
-      <LoadingSpinner/>
+      <section className='hero'>
+        <SearchBar/>
+      </section>
+      <section className=''>
+        <div className='review'>
+          <div><img src="" alt="" /></div>
+          <div>
+            <p>name</p>
+            <p>star</p>
+            <p>description</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
