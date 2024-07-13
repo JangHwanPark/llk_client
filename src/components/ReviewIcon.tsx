f6be928902330ccee987e144a89e8d8cbaad765f
user <wkd5878@gmail.com>
2024-07-13 15:24:04 +0900

    feat: 리뷰아이콘 기초 스타일링

--- src/components/ReviewIcon.tsx
+++ src/components/ReviewIcon.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 const ReviewIcon = () => {
   return (
     <div className='review'>
-      <div><img src="user.png" alt="" /></div>
-      <div>
-        <p>name</p>
+      <div className='review-info'>
         <p>star</p>
         <p>description</p>
+      </div>
+      <div className='user'>
+        <div className='user-image'>
+          <img src="user.png" alt="" />
+        </div>
+        <p>name</p>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,6 +12,8 @@ export default function Home() {
       </section>
       <section className='review-section'>
         <ReviewIcon/>
+        <ReviewIcon/>
+        <ReviewIcon/>
       </section>
     </div>
   );
