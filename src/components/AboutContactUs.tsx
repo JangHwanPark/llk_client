9b7ee4c4a442e48cb8f476d362842b1d672ae63e
geonKO <kws07042@gmail.com>
2024-07-16 18:33:07 +0900

    About.tsx page 생성 및 스타일링 AboutContactUs.tsx생성

---
+++ src/components/AboutContactUs.tsx
@@ -0,0 +1,95 @@
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import {InputField} from "./InputField";
+import {Button} from "./Button";
+import {postSubmitContactUs} from "../api/user-service";
+import {useMutation} from "@tanstack/react-query";
+import LoadingSpinner from "./LoadingSpinner";
+
+interface formDataType {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    description: string;
+}
+
+const ContactUsForm: React.FC = () => {
+    const mutation = useMutation({
+        mutationFn: postSubmitContactUs
+    })
+
+    const [formData, setFormData] = useState<formDataType>({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        description: ''
+    });
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (
+        e: FormEvent<HTMLFormElement>
+    ) => {
+        e.preventDefault();
+        mutation.mutate(formData);
+    }
+
+    return (
+        <form className='contact-form' onSubmit={handleSubmit}>
+            {mutation.isPending ? (
+                <LoadingSpinner/>
+            ) : (
+                <>
+                    <div className='text-wrapper'>
+                        <h2>Contact</h2>
+                        <InputField
+                            placeholder='first name'
+                            type='text'
+                            name='firstName'
+                            value={formData.firstName}
+                            onChange={handleChange}
+                        />
+                        <InputField
+                            placeholder='last name'
+                            type='text'
+                            name='lastName'
+                            value={formData.lastName}
+                            onChange={handleChange}
+                        />
+                        <InputField
+                            placeholder='email'
+                            type='text'
+                            name='email'
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <InputField
+                            placeholder='phone'
+                            type='text'
+                            name='phone'
+                            value={formData.phone}
+                            onChange={handleChange}
+                        />
+                        <textarea
+                            id="contact-description"
+                            name="description"
+                            placeholder='description'
+                            onChange={handleChange}
+                            defaultValue="I really enjoyed biking yesterday!"
+                        />
+                        <Button text='Submit' type='submit'/>
+                    </div>
+                </>
+            )}
+        </form>
+    );
+};
+
+export default ContactUsForm;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import Container from '../layout/Container';
+import AboutContactUsForm from '../components/AboutContactUs';
+import ReviewBox from '../components/ReviewBox';
 
 const About = () => {
   return (
-    <Container>
-    </Container>
+      <Container>
+        <div className="about-page">
+          <div className="self-introduction">
+            <div className="img-wrapper">
+              <img src="/img/hero02.jpg" className="profile-photo" alt="Wonjin Han" />
+            </div>
+            <div className="text-wrapper">
+              <h2>Wonjin Han</h2>
+              <p>Have a question for our agent?</p>
+            </div>
+          </div>
+          <div className="container">
+            <div className="about-contact">
+              <AboutContactUsForm />
+            </div>
+            <div className="about-review">
+              <div className="review">
+                <h2>Customer Reviews</h2>
+                <h3>See what our customers have to say</h3>
+                <ReviewBox
+                    customerName="Customer 1"
+                    review="it's so good and pantasic"
+                    rating={5}
+                />
+                <ReviewBox
+                    customerName="Customer 2"
+                    review="it's so good"
+                    rating={4}
+                />
+                <ReviewBox
+                    customerName="Customer 3"
+                    review="not bad"
+                    rating={2}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
   );
 };
 
