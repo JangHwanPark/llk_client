5b23a1d6d886819ecdb86e28210a63aed4ede7d2
user <wkd5878@gmail.com>
2024-07-16 09:32:27 +0900

    refactoring: 캐러셀 슬라이더 내 슬라이더 효과 추가

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,22 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Carousel = () => {
   const [index, setIndex] = useState(0);
-  const images = ['./img/hero01.jpg', './img/hero02.jpg', './img/hero03.jpg'];
+  const [animating, setAnimating] = useState(false);
+
+  const originalImage = ['./img/hero01.jpg', './img/hero02.jpg', './img/hero03.jpg'];
+  const images = [
+    originalImage[originalImage.length - 1],
+    ...originalImage,
+    originalImage[0]
+  ]
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIndex(prev => (prev + 1) % images.length);
-    }, 5000);
+    let timer: string | number | NodeJS.Timeout;
+    if (!animating) {
+      timer = setTimeout(() => {
+        setAnimating(true); // 애니메이션 시작
+        setIndex(prev => (prev + 1) % images.length);
+      }, 2000);
+    }
 
     return () => clearTimeout(timer);
-  }, [index])
+  }, [index, animating]);
+
+  const handleTransitionEnd = () => {
+    if (index === images.length - 1) {
+      setIndex(0);
+    } else if (index === 0) {
+      setIndex(images.length - 2);
+    }
+    setAnimating(false); // 애니메이션 끝
+  };
 
   return (
-    <div className='carousel-container'>
-      {images.map(img => (
-        <div key={img} style={{transform: `translateX(-${index * 100}%)`}}>
-          <img src={img} alt=""/>
+    <div className='carousel-container' onTransitionEnd={handleTransitionEnd}>
+      {images.map((img, idx) => (
+        <div
+          key={img}
+          style={{
+            transform: `translateX(-${index * 100}%)`,
+            transition: animating ? 'transform 1s ease-in-out' : 'none' // 애니메이션이 없을 때는 transition 적용 제거
+          }}>
+          <img src={img} alt={`Slide ${idx}`} style={{ width: '100%', height: 'auto' }} />
         </div>
       ))}
     </div>
