2d5031f05d3090dcd82343fbf5dfa841351dc32b
user <wkd5878@gmail.com>
2024-07-17 23:31:43 +0900

    refactoring: 메인 페이지 모바일 대응 (히어로, 네비게이션 제외)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,61 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import Container from '../layout/Container';
-import ContactUsForm from "../components/ContactUsForm";
-import Review from "../components/Review/Review";
-import SectionLayout from "../layout/HomeSection";
+import ContactUsForm from '../components/ContactUsForm';
+import Review from '../components/Review/Review';
+import SectionLayout from '../layout/HomeSection';
 
 const exploreData = [
-  {path: './img/home01.jpeg'},
-  {path: './img/home02.jpeg'},
-  {path: './img/home03.jpeg'},
-  {path: './img/home04.jpeg'},
-  {path: './img/home05.jpeg'},
-  {path: './img/home06.jpeg'},
-  {path: './img/home07.jpeg'},
-  {path: './img/home08.jpeg'},
-]
+  { path: './img/home01.jpeg' },
+  { path: './img/home02.jpeg' },
+  { path: './img/home03.jpeg' },
+  { path: './img/home04.jpeg' },
+  { path: './img/home05.jpeg' },
+  { path: './img/home06.jpeg' },
+  { path: './img/home07.jpeg' },
+  { path: './img/home08.jpeg' },
+];
 
 export default function Home() {
   return (
     <Container>
-      <section className='hero'>
+      <section className="hero">
         <SearchBar />
       </section>
       <SectionLayout
-        className='review-section'
-        title='what our clients says'>
-        <Review/>
+        className="review-section"
+        title="what our clients says">
+        <Review />
       </SectionLayout>
       <SectionLayout
-        className='explore-section'
-        title='Explore'>
-        <div className='explore-container'>
+        className="explore-section"
+        title="Explore">
+        <div className="explore-container">
           {exploreData.map((item, index) => (
             <div key={index} className="explore-item">
-              <img src={item.path} alt=""/>
+              <img src={item.path} alt="" />
             </div>
           ))}
         </div>
       </SectionLayout>
       <SectionLayout
         className='company-section'
+        title='WHY WORK WITH US?'>
+        <div>WHY WORK WITH US?</div>
+      </SectionLayout>
+      {/* News Pending */}
+      {/*<SectionLayout
+        className='company-section'
         title='News'>
         <div>부동산 뉴스.</div>
+      </SectionLayout>*/}
+      <SectionLayout
+        className='contact-us'>
+        <div className='img-wrapper'>
+          <img src="./img/hero02.jpg" alt="" />
+        </div>
+        <ContactUsForm />
       </SectionLayout>
-      <section>
-        <ContactUsForm/>
-      </section>
     </Container>
   );
 }
