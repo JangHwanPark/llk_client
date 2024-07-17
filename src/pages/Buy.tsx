118d8b717997e94d06c79470e1642abfc74d815e
japark <wkd5878@gmail.com>
2024-07-17 17:57:12 +0900

    feat: buy 내 검색 컴포넌트 배치

--- src/pages/Buy.tsx
+++ src/pages/Buy.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
 import Container from "../layout/Container";
+import SearchBar from "../components/SearchBar";
 
 const Buy = () => {
   return (
     <Container>
+      <SearchBar/>
       <Map
-        style={{width: '600px', height: '600px'}}
+        style={{width: '100%', height: '100%'}}
         defaultZoom={13}
         defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
         onCameraChanged={ (ev: MapCameraChangedEvent) =>
