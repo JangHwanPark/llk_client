dd3d8fe2da958caaa208b08a16425b3b5cd49715
geonKO <kws07042@gmail.com>
2024-07-17 13:48:45 +0900

    태그 시멘틱 작업

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,46 +3,46 @@ import Container from '../layout/Container';
 import AboutContactUsForm from '../components/AboutContactUs';
 import ReviewBox from '../components/ReviewBox';
 
-const About = () => {
+const About: React.FC = () => {
   return (
       <Container>
-        <div className="about-page">
-          <div className="self-introduction">
+        <section className="about-page">
+          <header className="self-introduction">
             <div className="img-wrapper">
-              <img src="/img/hero02.jpg" className="profile-photo" alt="Wonjin Han" />
+              <img src="/img/geon.jpg" className="profile-photo" alt="Wonjin Han" />
             </div>
             <div className="text-wrapper">
               <h2>Wonjin Han</h2>
               <p>Have a question for our agent?</p>
             </div>
-          </div>
-          <div className="container">
-            <div className="about-contact">
+          </header>
+          <section  className="container">
+            <section className="about-contact">
               <AboutContactUsForm />
-            </div>
-            <div className="about-review">
+            </section>
+            <section className="about-review">
               <div className="review">
                 <h2>Customer Reviews</h2>
                 <h3>See what our customers have to say</h3>
                 <ReviewBox
                     customerName="Customer 1"
-                    review="it's so good and pantasic"
+                    review="It's so good and fantastic"
                     rating={5}
                 />
                 <ReviewBox
                     customerName="Customer 2"
-                    review="it's so good"
+                    review="It's so good"
                     rating={4}
                 />
                 <ReviewBox
                     customerName="Customer 3"
-                    review="not bad"
+                    review="Not bad"
                     rating={2}
                 />
               </div>
-            </div>
-          </div>
-        </div>
+            </section>
+          </section>
+        </section>
       </Container>
   );
 };
