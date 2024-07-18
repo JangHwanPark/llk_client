244a965681a215a2850fb41b2308d89494da6c1c
geonKO <kws07042@gmail.com>
2024-07-19 04:29:20 +0900

    Revert "feat SellForm.tsx 추가"
    This reverts commit e3269bc2c5862da42c325ed691ba81a859f2992f.

--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
 import Container from "../layout/Container";
-import SellForm from "../components/SellForm";
 
 const Sell = () => {
   return (
     <Container>
       <Navigation/>
-        <SellForm />
     </Container>
   );
 };
