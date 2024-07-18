678555cbc3d571e873dab9bc056233e96dd2fce0
geonKO <kws07042@gmail.com>
2024-07-18 18:11:30 +0900

    feat SellForm.tsx 추가

--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
 import Container from "../layout/Container";
+import SellForm from "../components/SellForm";
 
 const Sell = () => {
   return (
     <Container>
       <Navigation/>
+        <SellForm />
     </Container>
   );
 };
